ae047e7ad2b2402b9d88014c18f29b4ac758350b
Jesse <jeslelacodes@gmail.com>
2024-09-10 12:58:10 +0100

    UI changes

--- components/shared/Hero.jsx
+++ components/shared/Hero.jsx
@@ -4,19 +4,18 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <section className="container mt-16 flex min-h-screen flex-col items-center">
-      <p className="rounded-full border border-dashed border-gray-400 px-6 py-2 font-bold text-muted-foreground">
-        miniboard
+    <section className="mt-14 flex min-h-screen flex-col items-center px-10 lg:container">
+      <p className="rounded-full border border-dashed border-gray-400 px-6 py-2 text-sm font-bold text-muted-foreground">
+        miniboard 📰
       </p>
-      <h1 className="leading-12 mt-6 px-5 text-center text-5xl font-bold tracking-tighter md:text-7xl">
-        Promote your event like a <span className="highlight-2">pro</span>
+      <h1 className="leading-12 mt-6 text-center text-4xl font-bold tracking-tighter md:text-7xl">
+        Promote your event like a <span className="highlight">pro</span>
       </h1>
-      <p className="mt-6 px-5 text-center text-xl tracking-tight text-[#010D3E]">
+      <p className="mt-6 text-center text-lg tracking-tight text-[#010D3E]">
         Effortlessly create WhatsApp-ready event captions with reminders for
         your guests
       </p>
-
-      <Button asChild className="mx-auto mt-6">
+      <Button asChild className="mx-auto mt-6" size="lg">
         <Link href="/events/create">Get started 🚀</Link>
       </Button>
     </section>
