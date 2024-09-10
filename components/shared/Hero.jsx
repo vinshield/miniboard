b0e866f34235b28985ca8b7fe495c0b9e4026a79
Jesse <jeslelacodes@gmail.com>
2024-09-10 17:07:50 +0100

    Revert "text change to hero"
    This reverts commit b853f5df05e8c2f5700ca11cf7dd7271f982b17b.

--- components/shared/Hero.jsx
+++ components/shared/Hero.jsx
@@ -5,25 +5,21 @@ import EventForm from "./EventForm";
 
 const Hero = () => {
   return (
-    <>
-      <section className="mt-14 flex flex-col items-center px-10 lg:container">
-        <p className="rounded-full border border-dashed border-gray-400 px-6 py-[6px] text-xs font-bold text-muted-foreground">
-          miniboard 📰
-        </p>
-        <h1 className="leading-12 mt-6 text-center text-5xl font-bold tracking-tighter md:text-7xl">
-          Promote your event like a <span className="highlight-2">pro</span>
-        </h1>
-        <p className="mt-6 text-center text-base tracking-tight text-[#010D3E]">
-          Effortlessly create WhatsApp-ready event captions with reminders for
-          your attendees
-        </p>
-        <Button asChild className="mx-auto mt-6">
-          <Link href="/events/create">Get started 🚀</Link>
-        </Button>
-      </section>
-
-      <EventForm />
-    </>
+    <section className="mt-14 flex min-h-screen flex-col items-center px-10 lg:container">
+      <p className="rounded-full border border-dashed border-gray-400 px-6 py-[6px] text-xs font-bold text-muted-foreground">
+        miniboard 📰
+      </p>
+      <h1 className="leading-12 mt-6 text-center text-5xl font-bold tracking-tighter md:text-7xl">
+        Promote your event like a <span className="highlight-2">pro</span>
+      </h1>
+      <p className="mt-6 text-center text-base tracking-tight text-[#010D3E]">
+        Effortlessly create WhatsApp-ready event captions with reminders for
+        your attendees
+      </p>
+      <Button asChild className="mx-auto mt-6">
+        <Link href="/events/create">Get started 🚀</Link>
+      </Button>
+    </section>
   );
 };
 
