66ed1e321821b18041f6a63e7ed0ba40b4906424
Jesse <jeslelacodes@gmail.com>
2024-09-10 13:17:42 +0100

    ui changes

--- components/shared/Hero.jsx
+++ components/shared/Hero.jsx
@@ -4,14 +4,14 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <section className="mt-14 flex min-h-screen flex-col items-center px-10 lg:container">
-      <p className="rounded-full border border-dashed border-gray-400 px-6 py-2 text-sm font-bold text-muted-foreground">
+    <section className="mt-20 flex min-h-screen flex-col items-center px-10 lg:container">
+      <p className="rounded-full border border-dashed border-gray-400 px-6 py-[6px] text-xs font-bold text-muted-foreground">
         miniboard 📰
       </p>
       <h1 className="leading-12 mt-6 text-center text-5xl font-bold tracking-tighter md:text-7xl">
-        Promote your event like a <span className="highlight">pro</span>
+        Promote your event like a <span className="highlight-2">pro</span>
       </h1>
-      <p className="mt-6 text-center text-lg tracking-tight text-[#010D3E]">
+      <p className="mt-6 text-center text-base tracking-tight text-[#010D3E]">
         Effortlessly create WhatsApp-ready event captions with reminders for
         your guests
       </p>
