4716cb62cab750389369cd6aa729ff728d3d53fc
Jesse <jeslelacodes@gmail.com>
2024-09-14 15:15:50 +0100

    test with com.google.calendar

--- app/(root)/add/page.jsx
+++ app/(root)/add/page.jsx
@@ -17,7 +17,7 @@ export default function Page() {
   return (
     <Link
       id="redirectButton"
-      href="intent://calendar.google.com/calendar/render?action=TEMPLATE&text=Movie%20Night%3A%20Guardians%20of%20the%20Galaxy%20%F0%9F%8E%AC&dates=20240928T183000/20240928T200000&ctz=Africa/Lagos&location=PTCF&details=Come%20and%20have%20a%20good%20time&reminder=30,60"
+      href="com.google.calendar://render?action=TEMPLATE&text=Movie%20Night%3A%20Guardians%20of%20the%20Galaxy%20%F0%9F%8E%AC&dates=20240928T183000/20240928T200000&ctz=Africa/Lagos&location=PTCF&details=Come%20and%20have%20a%20good%20time&reminder=30,60"
       style={{ display: "none" }}
     >
       Redirect
