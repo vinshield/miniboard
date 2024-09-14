f599976060d1e24030aa6fb7e63df0d749ba2643
Jesse <jeslelacodes@gmail.com>
2024-09-14 14:44:39 +0100

    Test add to calendar link using useEffect

--- app/(root)/add/page.jsx
+++ app/(root)/add/page.jsx
@@ -1,12 +1,20 @@
-// app/your-route/page.js
+"use client";
+
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Add({ searchParams }) {
   const { destination } = searchParams; // Get dynamic link from query params
 
-  const link =
-    destination ||
-    "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Movie%20Night%3A%20Guardians%20of%20the%20Galaxy%20%F0%9F%8E%AC&dates=20240928T183000/20240928T200000&ctz=Africa/Lagos&location=PTCF&details=Come%20and%20have%20a%20good%20time&reminder=30,60"; // Fallback to a default link if no query param
+  useEffect(() => {
+    // Specify the link you want to redirect to
+    const defaultLink =
+      "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Movie%20Night%3A%20Guardians%20of%20the%20Galaxy%20%F0%9F%8E%AC&dates=20240928T183000/20240928T200000&ctz=Africa/Lagos&location=PTCF&details=Come%20and%20have%20a%20good%20time&reminder=30,60";
 
-  redirect(link);
+    // Redirect to the external link
+    window.location.href = destination || defaultLink;
+
+    // OR if you're redirecting within your app, you can use:
+    // router.push(externalLink);
+  }, []);
 }
