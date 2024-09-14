f42c2d17af43fee8b366cce59faa50c92986ae51
Jesse <jeslelacodes@gmail.com>
2024-09-14 17:46:32 +0100

    Revert "Test add to calendar link by simulating button click"
    This reverts commit e0b3c8d0e992ac593a4c2b75cc688f7994821fa3.

--- app/(root)/add/page.jsx
+++ app/(root)/add/page.jsx
@@ -7,14 +7,31 @@ export default function Add({ searchParams }) {
   const { destination } = searchParams; // Get dynamic link from query params
 
   useEffect(() => {
-    // Find the button or link by its ID or any other selector
-    const button = document.getElementById("redirectButton");
+    // Specify the link you want to redirect to
+    const defaultLink =
+      "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Movie%20Night%3A%20Guardians%20of%20the%20Galaxy%20%F0%9F%8E%AC&dates=20240928T183000/20240928T200000&ctz=Africa/Lagos&location=PTCF&details=Come%20and%20have%20a%20good%20time&reminder=30,60";
 
-    if (button) {
-      // Simulate a click event
-      button.click();
-    }
+    // Redirect to the external link
+    window.location.href = destination || defaultLink;
+
+    // OR if you're redirecting within your app, you can use:
+    // router.push(externalLink);
   }, []);
 
-  return null; // No need to render anything
+  return (
+    <a
+      id="redirectButton"
+      href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Movie%20Night%3A%20Guardians%20of%20the%20Galaxy%20%F0%9F%8E%AC&dates=20240928T183000/20240928T200000&ctz=Africa/Lagos&location=PTCF&details=Come%20and%20have%20a%20good%20time&reminder=30,60"
+      style={{ display: "none" }}
+    >
+      Redirect
+    </a>
+  );
+}
+
+{
+  /* <h1>This page will redirect soon...</h1> */
+}
+{
+  /* Hidden button or link */
 }
