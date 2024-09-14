a717604590f4aa15033a680ded9aea7e66fb622d
Jesse <jeslelacodes@gmail.com>
2024-09-14 17:42:14 +0100

    Revert "Test add to calendar link by simulating button click"
    This reverts commit e0b3c8d0e992ac593a4c2b75cc688f7994821fa3.

--- app/(root)/add/page.jsx
+++ app/(root)/add/page.jsx
@@ -1,13 +1,19 @@
 "use client";
 
+import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
-export default function Page() {
-  useEffect(() => {
-    // Redirect to an external link
+export default function Add({ searchParams }) {
+  const { destination } = searchParams; // Get dynamic link from query params
 
-    window.location.href =
-      "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Movie%20Night%3A%20Guardians%20of%20the%20Galaxy%20%F0%9F%8E%AC&dates=20240928T183000/20240928T200000&ctz=Africa/Lagos&location=PTCF&details=Come%20and%20have%20a%20good%20time&reminder=30,60";
+  useEffect(() => {
+    // Find the button or link by its ID or any other selector
+    const button = document.getElementById("redirectButton");
+
+    if (button) {
+      // Simulate a click event
+      button.click();
+    }
   }, []);
 
   return null; // No need to render anything
