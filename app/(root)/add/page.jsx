13e74495674ab8212dea35683baf0e53d406ef46
Jesse <jeslelacodes@gmail.com>
2024-09-14 17:38:32 +0100

    switch back to useEffect

--- app/(root)/add/page.jsx
+++ app/(root)/add/page.jsx
@@ -1,33 +1,14 @@
 "use client";
 
-import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Page() {
   useEffect(() => {
-    // Find the button or link by its ID or any other selector
-    const button = document.getElementById("redirectButton");
+    // Redirect to an external link
 
-    if (button) {
-      // Simulate a click event
-      button.click();
-    }
+    window.location.href =
+      "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Movie%20Night%3A%20Guardians%20of%20the%20Galaxy%20%F0%9F%8E%AC&dates=20240928T183000/20240928T200000&ctz=Africa/Lagos&location=PTCF&details=Come%20and%20have%20a%20good%20time&reminder=30,60";
   }, []);
 
-  return (
-    <Link
-      id="redirectButton"
-      href="com.google.android.calendar://event?action=TEMPLATE&text=Movie%20Night%3A%20Guardians%20of%20the%20Galaxy%20%F0%9F%8E%AC&dates=20240928T183000/20240928T200000&ctz=Africa/Lagos&location=PTCF&details=Come%20and%20have%20a%20good%20time&reminder=30,60"
-      style={{ display: "none" }}
-    >
-      Redirect
-    </Link>
-  );
-}
-
-{
-  /* <h1>This page will redirect soon...</h1> */
-}
-{
-  /* Hidden button or link */
+  return null; // No need to render anything
 }
