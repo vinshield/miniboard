8c7d7f857032b9e8f0313d0efae489abde140015
Jesse <jeslelacodes@gmail.com>
2024-09-24 20:04:12 +0100

    test description in add to calendar button

--- app/(root)/add/page.jsx
+++ app/(root)/add/page.jsx
@@ -18,6 +18,7 @@ export default function Page() {
               images={[
                 "https://firebasestorage.googleapis.com/v0/b/wknd-47e66.appspot.com/o/event-posters%2FMovie%20-%20ac23e24a-d4a5-4aa8-82ec-37cc0403b81e?alt=media&token=3fe092cc-9602-43fa-99b0-96fb2f6498c2",
               ]}
+              description="[/p]Visit [url]https://miniboard-flax.vercel.app/add[/url] for more information."
               name="Movie Night: Guardians of the Galaxy 🎬"
               options={["Apple", "Google", "iCal", "Microsoft365"]}
               location="World Wide Web"
