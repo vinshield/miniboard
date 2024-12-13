a9dfe0bee086412b6183172bafc01a810109bf39
Jesse <jeslelacodes@gmail.com>
2024-12-13 22:50:06 +0100

    Update header appearance

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
 
           <div>
             <SignedOut>
-              <div className="rounded-full bg-[#7480911a] px-4 py-2 text-sm text-[#474b51] hover:bg-slate-400/40">
+              <div className="rounded-full bg-[#7480911a] px-4 py-2 text-sm font-semibold text-[#474b51] hover:bg-slate-400/40">
                 <SignInButton />
               </div>
             </SignedOut>
