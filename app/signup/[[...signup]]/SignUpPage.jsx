0d9dbfaaeacd0656918a459ec6329210b614a729
Jesse <jeslelacodes@gmail.com>
2024-12-25 17:52:10 +0100

    update ui

--- app/signup/[[...signup]]/SignUpPage.jsx
+++ app/signup/[[...signup]]/SignUpPage.jsx
@@ -61,7 +61,7 @@ export function SignUpPage({ username, onSignUpComplete, changeUsername }) {
   };
 
   return (
-    <div className="flex h-[100vh] flex-col justify-center">
+    <div className="mt-12 flex flex-col justify-center">
       <ArrowLeft className="mb-20" onClick={changeUsername} />
       {username && (
         <p className="mb-2 text-sm text-grey-500">
