95e2685cf3dc59f087be321a3d42e1b1a30eb277
Jesse <jeslelacodes@gmail.com>
2025-01-08 09:52:19 +0100

    update middleware

--- middleware.js
+++ middleware.js
@@ -1,8 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/api/webhooks(.*)"],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
