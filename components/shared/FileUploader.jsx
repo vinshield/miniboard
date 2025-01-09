74fceae97700062871e1aedc9bb38e6d0d585456
Jesse <jeslelacodes@gmail.com>
2025-01-09 17:45:23 +0100

    Fix for updated uploadthing

--- components/shared/FileUploader.jsx
+++ components/shared/FileUploader.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCallback, Dispatch, SetStateAction, useState } from "react";
-import { useDropzone } from "@uploadthing/react/hooks";
+import { useDropzone } from "@uploadthing/react";
 import { generateClientDropzoneAccept } from "uploadthing/client";
 import { extractPosterInfo } from "@/lib/actions/event.actions";
 
