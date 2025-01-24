3dca3ab79adfdba559aa67508383b7a503f23195
Jesse <jeslelacodes@gmail.com>
2025-01-24 17:14:25 +0100

    Update Footer component to conditionally render signup button based on user authentication status; adjust styling for improved spacing

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -1,21 +1,28 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
 import { TextShimmer } from "../ui/text-shimmer";
+import { useUser } from "@clerk/clerk-react";
 
 const Footer = () => {
+  const { user } = useUser();
   return (
-    <div className="flex-center container mt-16 flex flex-col bg-slate-100 pb-12 pt-12">
-      <Link asChild href="/signup">
-        <Button
-          variant="outline"
-          className="border border-sky-400 py-5 text-sm shadow-md"
-        >
-          <TextShimmer duration={2}>Create your miniboard</TextShimmer>
-        </Button>
-      </Link>
+    <div
+      className={`flex-center container mt-16 flex flex-col bg-slate-100 ${!user ? "pt-12" : ""} pb-12`}
+    >
+      {!user && (
+        <Link href="/signup">
+          <Button
+            variant="outline"
+            className="border border-sky-400 py-5 text-sm shadow-md"
+          >
+            <TextShimmer duration={2}>Create your miniboard</TextShimmer>
+          </Button>
+        </Link>
+      )}
 
-      <p className="mt-4 text-sm text-[#5a5e65]">
+      <p className="mt-8 text-xs text-[#757b85]">
         Built by{" "}
         <Link className="cursor-pointer underline underline-offset-4" href="/">
           miniboard
