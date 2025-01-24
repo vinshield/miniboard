46fccefcd3046c96566d42ab3f70ee54aa393801
Jesse <jeslelacodes@gmail.com>
2025-01-24 16:40:57 +0100

    Add TextShimmer component and integrate it into Footer; update styling for improved visual appeal

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
+import { TextShimmer } from "../ui/text-shimmer";
 
 const Footer = () => {
   return (
-    <div className="flex-center container mb-16 mt-16 flex flex-col">
-      <div className="mb-12 w-full border-t border-gray-300 shadow-md"></div>
+    <div className="flex-center container mt-16 flex flex-col bg-slate-100 pb-12 pt-12">
       <Link asChild href="/signup">
         <Button
           variant="outline"
-          className="border border-sky-400 py-5 text-sm text-[#5b6169] shadow-md"
+          className="border border-sky-400 py-5 text-sm shadow-md"
         >
-          Create your
-          <span className="font-bold">&nbsp;miniboard</span>
+          <TextShimmer duration={2}>Create your miniboard</TextShimmer>
         </Button>
       </Link>
-      <p className="mt-4 text-sm text-[#757b85]">
+
+      <p className="mt-4 text-sm text-[#5a5e65]">
         Built by{" "}
         <Link className="cursor-pointer underline underline-offset-4" href="/">
           miniboard
