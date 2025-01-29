04906d680448d7254046794e54c9bd8c4d05a4ec
Jesse <jeslelacodes@gmail.com>
2025-01-29 22:44:06 +0100

    Enhance loading component with tabbed navigation and improved skeleton layouts for events and blogs

--- app/(root)/[username]/loading.jsx
+++ app/(root)/[username]/loading.jsx
@@ -1,9 +1,10 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Loading = () => {
   return (
     <>
-      <div className="container my-5 flex flex-col items-start space-y-5">
+      <div className="container flex flex-col items-start space-y-5">
         {/* Profile Header */}
         <div className="flex flex-col gap-5">
           {/* Profile Image Skeleton */}
@@ -14,7 +15,7 @@ const Loading = () => {
         </div>
 
         {/* Bio Skeleton */}
-        <Skeleton className="h-10 w-full rounded-lg" />
+        <Skeleton className="h-16 w-4/5 rounded-lg" />
 
         {/* Social Handles Skeleton */}
         <div className="flex space-x-3">
@@ -24,17 +25,56 @@ const Loading = () => {
         </div>
       </div>
 
-      {/* Skeletons for Event Cards */}
-      <div className="container space-y-4">
-        {[1, 2, 3].map((index) => (
-          <div key={index} className="flex flex-col overflow-hidden rounded-lg">
-            <Skeleton className="h-5 w-32 rounded" /> {/* Event title */}
-            <Skeleton className="mt-2 h-4 w-24 rounded" /> {/* Date */}
-            <Skeleton className="mt-4 h-20 w-full rounded" />{" "}
-            {/* Description */}
+      <Tabs defaultValue="events">
+        <div className="container">
+          <TabsList className="my-5 grid grid-cols-2">
+            <TabsTrigger value="events">Events</TabsTrigger>
+            <TabsTrigger value="blogs">Blogs</TabsTrigger>
+          </TabsList>
+        </div>
+        <TabsContent value="events">
+          {/* Skeletons for Event Cards */}
+
+          <div className="mt container space-y-6">
+            {[1, 2, 3].map((index) => (
+              <div
+                key={index}
+                className="flex gap-10 overflow-hidden rounded-lg border border-primary/10 p-4"
+              >
+                <div className="flex-1">
+                  <Skeleton className="h-6 w-full rounded-lg" />{" "}
+                  {/* Event title */}
+                  <Skeleton className="mt-2 h-6 w-32 rounded-lg" /> {/* Date */}
+                </div>
+                <div>
+                  <Skeleton className="h-36 w-24 rounded-lg" />{" "}
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </TabsContent>
+        <TabsContent value="blogs">
+          {/* Skeletons for Event Cards */}
+
+          <div className="container mt-12 space-y-6">
+            {[1, 2, 3].map((index) => (
+              <div
+                key={index}
+                className="flex gap-10 overflow-hidden rounded-lg border border-primary/10 p-4"
+              >
+                <div className="flex-1">
+                  <Skeleton className="h-6 w-full rounded-lg" />{" "}
+                  {/* Event title */}
+                  <Skeleton className="mt-2 h-6 w-32 rounded-lg" /> {/* Date */}
+                </div>
+                <div>
+                  <Skeleton className="h-36 w-24 rounded-lg" />{" "}
+                </div>
+              </div>
+            ))}
+          </div>
+        </TabsContent>
+      </Tabs>
     </>
   );
 };
