43f12e843be87872940cbe50d124459ada96d500
Jesse <jeslelacodes@gmail.com>
2025-02-28 08:56:20 +0100

    Refactor SignInPage layout for improved styling and structure

--- app/signin/[[...signin]]/SignInPage.jsx
+++ app/signin/[[...signin]]/SignInPage.jsx
@@ -64,112 +64,114 @@ export function SignInPage() {
   };
 
   return (
-    <div className="mt-4 flex flex-col justify-center">
-      <p className="mb-1 text-3xl text-gray-500">Welcome back!</p>
-      <h2 className="leading-12 mb-16 block text-4xl font-semibold tracking-tighter md:text-4xl">
-        Sign in to your Miniboard
-      </h2>
+    <div className="flex flex-col space-y-16">
+      <div className="mt-12">
+        <p className="mb-1 text-3xl text-gray-500">Welcome back!</p>
+        <h2 className="leading-12 block text-4xl font-semibold tracking-tighter md:text-4xl">
+          Sign in to your Miniboard
+        </h2>
+      </div>
 
-      <div className="w-full max-w-md space-y-8">
-        <div className="my-4 grid gap-y-4">
-          <div className="grid grid-cols-2 gap-x-2">
-            <Button
-              variant="outline"
-              type="button"
-              disabled={!isLoaded}
-              onClick={() => handleOAuthSignIn("oauth_google")}
-            >
-              {isLoaded ? (
-                <div className="flex items-center gap-x-2">
-                  <i className="ci ci-google ci-1x"></i>
-                  Google
-                </div>
-              ) : (
-                <LoaderCircle className="size-4 animate-spin" />
-              )}
-            </Button>
-            <Button
-              variant="outline"
-              type="button"
-              disabled={!isLoaded}
-              onClick={() => handleOAuthSignIn("oauth_tiktok")}
-            >
-              {isLoaded ? (
-                <div className="flex items-center gap-x-2">
-                  <i className="ci ci-tiktok ci-1x"></i>
-                  TikTok
-                </div>
-              ) : (
-                <LoaderCircle className="size-4 animate-spin" />
-              )}
-            </Button>
+      <div className="flex w-full max-w-md flex-col gap-y-4">
+        <div className="grid grid-rows-2 gap-y-2">
+          <Button
+            size="lg"
+            variant="outline"
+            type="button"
+            disabled={!isLoaded}
+            onClick={() => handleOAuthSignIn("oauth_google")}
+          >
+            {isLoaded ? (
+              <div className="flex items-center gap-x-2">
+                <i className="ci ci-google ci-1x"></i>
+                Sign in with Google
+              </div>
+            ) : (
+              <LoaderCircle className="size-4 animate-spin" />
+            )}
+          </Button>
+          <Button
+            size="lg"
+            variant="outline"
+            type="button"
+            disabled={!isLoaded}
+            onClick={() => handleOAuthSignIn("oauth_tiktok")}
+          >
+            {isLoaded ? (
+              <div className="flex items-center gap-x-2">
+                <i className="ci ci-tiktok ci-1x"></i>
+                Sign in with TikTok
+              </div>
+            ) : (
+              <LoaderCircle className="size-4 animate-spin" />
+            )}
+          </Button>
+        </div>
+
+        <p className="flex items-center gap-x-3 text-sm text-muted-foreground before:h-px before:flex-1 before:bg-border after:h-px after:flex-1 after:bg-border">
+          or
+        </p>
+
+        <form onSubmit={handleSubmit} className="space-y-2">
+          <div>
+            <label htmlFor="email-or-username" className="sr-only">
+              Email or username
+            </label>
+            <Input
+              className="input-field min-w-[20px] rounded-lg px-4 py-6 text-base placeholder:text-[#a8a8a8]"
+              type="text"
+              placeholder="Email or username"
+              value={identifier}
+              onChange={(e) => setIdentifier(e.target.value)}
+              required
+              autoComplete="off"
+            />
           </div>
-
-          <p className="flex items-center gap-x-3 text-sm text-muted-foreground before:h-px before:flex-1 before:bg-border after:h-px after:flex-1 after:bg-border">
-            or
-          </p>
-
-          <form onSubmit={handleSubmit} className="space-y-2">
-            <div>
-              <label htmlFor="email-or-username" className="sr-only">
-                Email or username
-              </label>
-              <Input
-                className="input-field min-w-[20px] rounded-lg px-4 py-6 text-base placeholder:text-[#a8a8a8]"
-                type="text"
-                placeholder="Email or username"
-                value={identifier}
-                onChange={(e) => setIdentifier(e.target.value)}
-                required
-                autoComplete="off"
-              />
-            </div>
-            <div className="space-y-1">
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <Input
-                className="input-field min-w-[20px] rounded-lg px-4 py-6 text-base placeholder:text-[#a8a8a8]"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                autoComplete="off"
-              />
-              {/* <div className="flex justify-end">
+          <div className="space-y-1">
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <Input
+              className="input-field min-w-[20px] rounded-lg px-4 py-6 text-base placeholder:text-[#a8a8a8]"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              autoComplete="off"
+            />
+            {/* <div className="flex justify-end">
                 <Button variant="link" size="sm" className="text-xs" asChild>
                   <Link href="/forgot-password">Forgot password?</Link>
                 </Button>
               </div> */}
-            </div>
-            {error && <p className="text-sm text-red-500">{error}</p>}
-            <Button
-              type="submit"
-              disabled={!isLoaded || isLoading}
-              size="lg"
-              variant="test"
-              className="min-w-2/5 mt-6 w-full rounded-lg"
-            >
-              {isLoading ? (
-                <LoaderCircle className="size-4 animate-spin" />
-              ) : (
-                "Sign In"
-              )}
-            </Button>
-            <div className="text-center text-sm">
-              <Button variant="link" size="sm" className="mt-14" asChild>
-                <Link href="signup">
-                  <span className="text-gray-500">
-                    Don&apos;t have an account?
-                  </span>
+          </div>
+          {error && <p className="text-sm text-red-500">{error}</p>}
+          <Button
+            type="submit"
+            disabled={!isLoaded || isLoading}
+            size="lg"
+            variant="test"
+            className="min-w-2/5 mt-6 w-full rounded-lg"
+          >
+            {isLoading ? (
+              <LoaderCircle className="size-4 animate-spin" />
+            ) : (
+              "Sign In"
+            )}
+          </Button>
+          <div className="text-center text-sm">
+            <Button variant="link" size="sm" className="mt-8" asChild>
+              <Link href="signup">
+                <span className="text-gray-500">
+                  Don&apos;t have an account?
+                </span>
 
-                  <span className="font-bold">&nbsp;Sign up</span>
-                </Link>
-              </Button>
-            </div>
-          </form>
-        </div>
+                <span className="font-bold">&nbsp;Sign up</span>
+              </Link>
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- app/signin/[[...signin]]/page.jsx
+++ app/signin/[[...signin]]/page.jsx
@@ -2,7 +2,7 @@ import { SignInPage } from "./SignInPage";
 
 export default function Page() {
   return (
-    <div className="container flex min-h-[calc(100vh-4rem)] flex-col items-center justify-center py-10">
+    <div className="container flex flex-col items-center justify-center">
       <SignInPage />
     </div>
   );
